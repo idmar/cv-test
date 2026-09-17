d1b4bc9c66630292f6f48c419ae43b3298cca1e4
idmar <idmar@users.noreply.github.com>
2026-09-17 14:28:52 +0800

    增加自动部署脚本

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const base = process.env.GITHUB_ACTIONS && repositoryName && !repositoryName.endsWith('.github.io')
+  ? `/${repositoryName}/`
+  : '/';
+
 export default defineConfig({
+  base,
   plugins: [react()],
   resolve: {
     alias: {
